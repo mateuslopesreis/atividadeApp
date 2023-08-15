00d868c797c2bb34120761707f10778f4949a65c
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2023-08-15 19:30:26 -0300

    criando app tela home e cadastro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private toast: ToastController, public nav: NavController) {}
+
+
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toast.create({
+      message: 'Bem vindo ',
+      duration: 2000,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+  abrirPagina(x: string){
+    //console.log("Você clicou no botão");
+    this.nav.navigateForward(x);
+
+  }
+
+
+
+  ngOnInit() {
+  }
+
 
 }
